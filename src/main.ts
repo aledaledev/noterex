629cb49d0b6e963c343eee1cb9af611e1620ce9d
aledale <ale.salas271102@gmail.com>
2022-12-07 23:58:55 -0300

    getting input values

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import './styles/style.css'
+import {v4 as uuid} from 'uuid'
+
+type Task = {
+    id: string,
+    title: string,
+    description: string
+    date: Date
+}
+
+let tasks:Task[] = []
 
 const taskForm = document.getElementById('task-form')
 taskForm?.addEventListener('submit', (e) => {
     e.preventDefault()
     const title = taskForm['title'] as unknown as HTMLInputElement
     const description = taskForm["description"] as HTMLTextAreaElement
-    console.log(title.value);
-    console.log(description.value);
+    const task:Task = {
+        id: uuid(),
+        title: title.value,
+        description: description.value,
+        date: new Date()
+    }
+    tasks = [...tasks, task]
+    console.log(tasks);
     
 })
 
